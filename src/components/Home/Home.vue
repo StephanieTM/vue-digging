5683c88c05798fed679850fad629c65df54f37d1
Stephanie.Gao <tm1997@163.com>
2021-06-23 16:19:00 +0800

    feat: init vue

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    vue digging
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<style scoped>
+.home {
+  background: lavender;
+  border-radius: 20px;
+  padding: 20px;
+  margin: 20px;
+  font-weight: 600;
+  font-size: 24px;
+}
+</style>
