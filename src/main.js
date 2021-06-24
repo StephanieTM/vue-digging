ba11740b28e96cbda5bbf584d5a785acdbb4ef30
Stephanie.Gao <tm1997@163.com>
2021-06-24 17:11:44 +0800

    feat: organize router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+  },
+  {
+    path: '/hello-world',
+    component: () => import('./components/HelloWorld'),
+  },
+  {
+    path: '/tutorial',
+    component: () => import('./tutorial'),
+    children: [
+      {
+        path: '1',
+        component: () => import('./tutorial/01-declarative-rendering'),
+        children: [
+          {
+            path: '',
+            component: () => import('./components/Placeholder'),
+          },
+          {
+            path: 'counter',
+            component: () => import('./tutorial/01-declarative-rendering/Counter'),
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App)
+  .use(router)
+  .mount('#app');
