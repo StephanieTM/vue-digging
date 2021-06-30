67514cfaf1423a5aff8302a0dd0c702cdb0ffeda
Stephanie.Gao <tm1997@163.com>
2021-06-30 16:49:37 +0800

    feat: v-for with component

---
+++ src/tutorial/01-essentials/list-rendering/todo-item.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    [ID: {{ id }}] {{title}}
+    <el-button type="text" icon="el-icon-delete" @click="$emit('remove')"></el-button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    id: Number,
+  },
+  emits: ['remove'],
+}
+</script>
