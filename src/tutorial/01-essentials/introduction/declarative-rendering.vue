bf14aa8f091d706146f9ab02fbc950eef9650f4b
Stephanie.Gao <tm1997@163.com>
2021-06-30 10:57:45 +0800

    feat: optimize counter

--- src/tutorial/01-essentials/introduction/declarative-rendering.vue
+++ src/tutorial/01-essentials/introduction/declarative-rendering.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="counter">
-    Counter: {{ counter }}
+  <div>
+    <span class="counter-text">Counter: {{ counter }}</span>
+    <el-button @click="toggleCounter">{{isCounting ? 'Stop' : 'Start'}}</el-button>
   </div>
 </template>
 
@@ -9,12 +10,44 @@ export default {
   data() {
     return {
       counter: 0,
+      intervalHandle: undefined,
+      isCounting: false,
     };
   },
   mounted() {
-    setInterval(() => {
-      this.counter++;
-    }, 1000);
+    this.startCounter();
+  },
+  beforeUnmount() {
+    this.stopCounter();
+  },
+  methods: {
+    stopCounter() {
+      if (this.isCounting) {
+        clearInterval(this.intervalHandle);
+        this.isCounting = false;
+      }
+    },
+    startCounter() {
+      if (!this.isCounting) {
+        this.intervalHandle = setInterval(() => {
+          this.counter++;
+        }, 1000);
+        this.isCounting = true;
+      }
+    },
+    toggleCounter() {
+      if (this.isCounting) {
+        this.stopCounter();
+      } else {
+        this.startCounter();
+      }
+    },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.counter-text {
+  margin-right: 20px;
+}
+</style>
