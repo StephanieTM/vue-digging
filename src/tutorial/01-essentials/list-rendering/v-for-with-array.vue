7fc7f9e98ceb42bf77794f0025132b385f975239
Stephanie.Gao <tm1997@163.com>
2021-06-30 16:49:16 +0800

    feat: v-for with array/object/range

---
+++ src/tutorial/01-essentials/list-rendering/v-for-with-array.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <!-- 可以用 of 或 in -->
+    <!-- <li v-for="(todo, index) of todoList" :key="todo.id"> -->
+    <li v-for="(todo, index) in todoList" :key="todo.id">
+      {{index+1}} {{todo.content}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoList: [
+        { id: '001', content: 'Meet Cindy at Café at 9 tonight.' },
+        { id: '002', content: 'Do the dishes tomorrow morning.' },
+        { id: '003', content: 'Finish the report by Mar 17th.' },
+      ],
+    };
+  },
+}
+</script>
---
+++ src/tutorial/01-essentials/list-rendering/v-for-with-range.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    1-10: <span v-for="n in 10" :key="n" class="number">{{n}}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.number {
+  margin: 5px;
+
+  &:not(:last-child)::after {
+    content: ',';
+  }
+}
+</style>
